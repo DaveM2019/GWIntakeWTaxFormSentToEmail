662c5192747a29867d6867791be70cf0e59002e7
Dave Mayfield <52716130+DaveM2019@users.noreply.github.com>
2019-08-15 19:11:40 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+var currentButton = -1;
+// To move to local/session storage, change the array into a stored object and use functions to get/set the values.
+var inputValues = [0,0,0,0,0,0,0,0,0,0,0,0];
+
+function goToButtonView(btnNumber) {
+  $("#mainview").css("visibility", "hidden");
+  $("#btnview").css("visibility", "visible");
+  $("#mainview").css("display", "none");
+  $("#btnview").css("display", "block");
+  $("#resultsview").css("display", "none");
+  $("#resultsview").css("visibility", "hidden");
+
+  currentButton = btnNumber;
+  $("#buttonInput")[0].value = inputValues[currentButton];
+}
+
+function returnToMainView() {
+  $("#mainview").css("visibility", "visible");
+  $("#btnview").css("visibility", "hidden");
+  $("#mainview").css("display", "block");
+  $("#btnview").css("display", "none");
+  $("#resultsview").css("display", "none");
+  $("#resultsview").css("visibility", "hidden");
+}
+
+function updateCurrentValue() {
+  let num = parseFloat($("#buttonInput")[0].value);
+
+  if (num >= 0) {
+  inputValues[currentButton] = num;
+  }
+}
+
+function showResults() {
+  $("#mainview").css("visibility", "hidden");
+  $("#btnview").css("visibility", "hidden");
+  $("#mainview").css("display", "none");
+  $("#btnview").css("display", "none");
+  $("#resultsview").css("display", "block");
+  $("#resultsview").css("visibility", "visible");
+
+
+  $("#resultText").text(JSON.stringify(inputValues));
+}
+
+//TEST
+//var button=document.getElementById("button1");
+//document.getElementById("button1");
+
+//button1.addEventListener("click") {
+//button1.addEventListener("click", function() {
+//button.addEventListener("click", function() {
+// button id="button1".addEventListener("click", function() {
+//    label.innerHTML="Quantity of clothes received:";
+//  }
+
+
+
+
+
+
+
+
+
+
+
+
+//END
